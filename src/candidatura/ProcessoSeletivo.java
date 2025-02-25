4777370dbed9994eb85d42284bb895d72de4cbfc
Vitu07 <vitormorais20092004@gmail.com>
2025-02-25 15:15:12 -0300

    ligações para candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,16 +1,43 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        imprimirSelecionados();
+        String [] candidatos = {"João", "Marcelo", "Maria", "Luiza", "Clara"};
+        for (String candidato : candidatos){
+            entrandoEmContato(candidato);
+        }
     }
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do{
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+
+
+        }while (continuarTentando && tentativasRealizadas<3);
+
+        if(atendeu)
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA");
+        else System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ", NÚMERO MÁXIMO DE TENTATIVAS " + tentativasRealizadas);
+    }
+
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
+    }
+
     static void imprimirSelecionados(){
         String [] candidatos = {"João", "Marcelo", "Maria", "Luiza", "Clara"};
         for (String candidato : candidatos){
             System.out.println("O candidato selecionado foi " + candidato);
         }
     }
-
 
     static void selecaoDeCandidatos() {
         //Array candidatos
