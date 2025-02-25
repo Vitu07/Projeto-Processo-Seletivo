d695e8b1548f30ac7139922c9f6415a0dc15f639
Vitu07 <vitormorais20092004@gmail.com>
2025-02-25 14:13:18 -0300

    selecionando candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,13 +1,31 @@
 package candidatura;
-
+import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoDeCandidatos();
     }
+    static void selecaoDeCandidatos() {
+        //Array candidatos
+        String [] candidatos = {"João", "Marcelo", "Maria", "Luiza", "Clara", "Roberto", "José", "Vera", "lais", "Larissa"};
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
 
-    static void analisarCandidato(double salarioPretendido){
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salário " + salarioPretendido );
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O candidatos " + candidato + " Foi selecionado para a vaga" );
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+    }
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+    static void analisarCandidato(double salarioPretendido) {
         double salarioBase= 2000.0;
         if(salarioBase > salarioPretendido){
             System.out.println("LIGAR PARA O CANDIDATO");
